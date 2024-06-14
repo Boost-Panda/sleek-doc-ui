f1d3f754b9e8257404902f0130ff80c96c83ca86
aliirz <dejavu87@gmail.com>
2024-06-14 19:12:09 +0500

    updated lauyout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <NavBar />
+    <!-- <NavBar /> -->
     <div class="min-h-screen">
       <!-- <header class="shadow">
         <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
